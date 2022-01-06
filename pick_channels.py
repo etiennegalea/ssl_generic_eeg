461baf68a91b67a1d06a441cadcc9d82937f863d
Etienne <egalea.11@gmail.com>
2022-01-06 14:17:00 +0100

    Added script to import, pick channels and export raw EEG files.

--- pick_channels.py
+++ pick_channels.py
@@ -2,19 +2,13 @@ import mne
 from mne_extras import write_edf
 
 import os
+from tqdm import tqdm
 
-
-data_dir = '/media/maligan/My Passport/ssl_thesis/data/SPACE_BAMBI'
+data_dir = '/media/maligan/My Passport/msc_thesis/ssl_thesis/data/SPACE_BAMBI'
 files = [os.path.join(data_dir, fname) for fname in os.listdir(data_dir)]
-print(files)
-
 
 # read, pick channels, save loop
-for file in files:
+for file in tqdm(files):
     raw = mne.io.read_raw_fif(f'{file}')
-    raw = raw.pick_channels(['Fpz', 'Pz'])
-    # raw.export(file, fmt='edf')
-
-    to_export = f"{file.split('/')[-1].split('.fif')[0]}.edf"
-    write_edf(raw, f"./picked_channels/{to_export}")
-    print(file)
+    to_export = f"{file.split('/')[-1]}"
+    raw.save(f"./data/SPACE_BAMBI_2channels/{to_export}", picks=['Fpz', 'Pz'], overwrite=True)
