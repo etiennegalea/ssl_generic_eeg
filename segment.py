afb20d211684256904eecea81d99037d19c077f9
Etienne <egalea.11@gmail.com>
2022-01-25 11:36:49 +0100

    Added Marina's segment.py script for segmenting SPACE/BAMBI windows accordingly

---
+++ segment.py
@@ -0,0 +1,150 @@
+""" Functions to:
+    1) Segment mne.Raw EEG recording and create mne.Epochs
+    2) Assign event ids to the created epochs based on existing annotation intervals in Raw.info.annotations
+
+    Example usage:
+    a) Preprocess the recording first (or not)
+    b) Segment the recording:
+    segments = _segment(mne.Raw, windowSize=WINDOW_SIZE, windowOverlap=WINDOW_OVERLAP)
+    c) Map segments to existing annotation intervals to assign event ids
+    segments_mapped = _map_artifacts(mne.Raw, segments, windowSize=WINDOW_SIZE, cutoffLength=CUTOFF_LENGTH)
+ """
+
+import mne
+import numpy as np
+import pandas as pd
+
+
+# Segmentation parameters (in seconds)
+WINDOW_SIZE = 1.0
+WINDOW_OVERLAP = 0.5
+# Mapping parameters (in seconds)
+CUTOFF_LENGTH = 0.1
+
+
+def _segment_metadata(Raw, events, windowSize):
+    # Ids and labels for artifacts (0), non-artifacts (1), and ignored (2)
+
+    event_idx = range(len(events))
+    ids = events[:, 2]
+    labels = np.where(np.array(ids) == 0, 'artifact', (np.where(np.array(ids) == 2, 'ignored', 'nonartifact')))
+    start = [onset[0]/Raw.info['sfreq'] for onset in events]
+    end = [onset + windowSize - 1./Raw.info['sfreq'] for onset in start]
+    sfreq = [Raw.info['sfreq']]*len(events)
+    filenames = [Raw._filenames[0]]*len(events)
+
+    # Create a dataframe
+    df = pd.DataFrame(list(zip(start, end, sfreq, labels, filenames)), index=event_idx,
+                      columns=['start', 'end', 'sfreq', 'true_label', 'filename'])
+
+    return df
+
+
+def _segment(Raw, windowSize, windowOverlap):
+    sfreq = Raw.info["sfreq"]
+    # Epoch length in timepoints/samples
+    epoch_length_timepoints = sfreq * windowSize
+    # Offset in seconds
+    epoch_offset_seconds = windowSize - windowSize * windowOverlap
+    # Offset in timepoints/samples
+    epoch_offset_timepoints = int(sfreq * epoch_offset_seconds)
+    # Make a set of events/segments separated by a fixed offset
+    n_epochs = int(np.ceil((Raw.__len__() - epoch_length_timepoints) / epoch_offset_timepoints + 1))
+    events = np.zeros((n_epochs, 3), dtype=int)
+    # Initiate event ids for the segments (label everything is non-artifact (1);
+    # will change to include artifacts (0) and ignored (2) when we map existing annotation intervals)
+    events[:, 2] = 1
+    events[:, 0] = np.array(
+        np.linspace(0, (n_epochs * epoch_offset_seconds) - epoch_offset_seconds, n_epochs) * sfreq,
+        dtype=int)
+    # Create metadata for segments
+    metadata = _segment_metadata(Raw, events, windowSize=windowSize)
+    # Create segments/mne epochs based on events
+    segments = mne.Epochs(Raw, events, event_id={'nonartifact': 1}, preload=True,
+                          tmin=0, tmax=windowSize - 1 / sfreq, metadata=metadata, baseline=None, verbose=0)
+    return segments
+
+
+def _map_artifacts(Raw, segments, windowSize, cutoffLength):
+    annotations_onsets = Raw.annotations.onset
+    annotations_durations = Raw.annotations.duration
+    event_onsets = np.array([onset[0] / Raw.info['sfreq'] for onset in segments.events])
+    mapped_segments = segments.copy()
+
+    for i, event_onset in enumerate(event_onsets):
+        for onset, duration in zip(annotations_onsets, annotations_durations):
+            # if segments within the annotation interval or intersects it to the right or left
+            # or annotation interval is within the segment
+            if (event_onset >= onset and event_onset + windowSize <= onset + duration) or \
+                    (
+                            event_onset >= onset and event_onset + windowSize >= onset + duration and event_onset < onset + duration) or \
+                    (
+                            event_onset <= onset and event_onset + windowSize > onset and event_onset + windowSize <= onset + duration) or \
+                    (
+                            event_onset <= onset and event_onset + windowSize > onset and event_onset + windowSize > onset + duration):
+
+                # Determine length of the intersection
+                if onset <= event_onset:
+                    start = event_onset
+                else:
+                    start = onset
+
+                if onset + duration >= event_onset + windowSize:
+                    end = event_onset + windowSize
+                else:
+                    end = onset + duration
+
+                intersection_length = end - start
+
+                # Call segment an artifact
+                if intersection_length >= cutoffLength:
+                    mapped_segments.events[i][2] = 0
+                    break
+                # Call segment an artifact
+                elif intersection_length < cutoffLength and duration <= cutoffLength:
+                    mapped_segments.events[i][2] = 0
+                    break
+                # Ignore segment
+                elif intersection_length < cutoffLength and duration > cutoffLength:
+                    mapped_segments.events[i][2] = 2
+            else:
+                continue
+    # Assign labels to ids
+    mapped_segments.event_id = {'nonartifact': 1, 'artifact': 0, 'ignored': 2}
+
+    return mapped_segments
+
+###########################################################################################
+######################################### EXAMPLE #########################################
+###########################################################################################
+
+# Input preprocessed recording
+infile_path = './data/BAMBI.S501.yyyymmdd.ECRASD1_raw.fif'
+# Load recording
+preprocessed_recording = mne.io.read_raw_fif(infile_path, preload=True)
+# Print existing annotations
+print(preprocessed_recording.info)
+print(preprocessed_recording.annotations)
+# Segment continuous EEG recording
+print('Segmenting ...')
+segments = _segment(preprocessed_recording, windowSize=WINDOW_SIZE, windowOverlap=WINDOW_OVERLAP)
+print('Number of generated segments is {}'.format(len(segments.events)))
+# Map created segments to existing annotation interval to assign event ids
+print('Mapping segments onto artifact annotation intervals ...')
+segments_mapped = _map_artifacts(preprocessed_recording, segments, windowSize=WINDOW_SIZE,
+                                    cutoffLength=CUTOFF_LENGTH)
+# Update metadata
+segments_mapped._metadata = _segment_metadata(preprocessed_recording, segments_mapped.events, WINDOW_SIZE)
+
+# Print results
+ids = [e[2] for e in segments_mapped.events]  # all ids
+idx_ignore = [id for id, id_ in enumerate(ids) if id_ == 2]  # ids of segments to ignore
+idx_artifacts = [id for id, id_ in enumerate(ids) if id_ == 0]  # ids of artifact segments
+idx_nonartifacts = [id for id, id_ in enumerate(ids) if id_ == 1]  # ids of nonartifact segments
+print('Number of artifact segments is {}'.format(len(idx_artifacts)))
+print('Number of non-artifact segments is {}'.format(len(idx_nonartifacts)))
+print('Number of segments to ignore is {}'.format(len(idx_ignore)))
+
+
+
+
