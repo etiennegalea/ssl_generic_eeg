cc0a2639d681b759e0ff97d75a6451d87665470b
Etienne <egalea.11@gmail.com>
2022-03-29 17:18:04 +0200

    modified learning curve plotting parameters

--- plot.py
+++ plot.py
@@ -232,8 +232,8 @@ class Plot:
         
         print(':: plotting learning curves (sklearn)... ', end='')
         plt.rc('font', size=26)
-        plt.rc('xtick', labelsize=26) 
-        plt.rc('ytick', labelsize=26)
+        plt.rc('xtick', labelsize=30) 
+        plt.rc('ytick', labelsize=30)
         plt.rc('axes', labelsize=26)
         _, ax = plt.subplots(1, 1, figsize=(20, 10))
         ax.grid()
@@ -257,12 +257,12 @@ class Plot:
         ax.set_xlabel("Training examples")
         ax.set_ylabel(scoring)
 
-        plt.title(f'Balanced accuracy per training example for {dataset_name} dataset', fontsize=26)
+        # plt.title(f'Balanced accuracy per training example for {dataset_name} dataset', fontsize=26)
         plt.plot(train_sizes, ssl_test_scores_mean, '-', color='r', label='SSL')
         plt.plot(train_sizes, raw_test_scores_mean, '-', color='g', label='FS')
-        plt.legend(loc="best")
+        # plt.legend(loc="best")
 
-        self._plot(plt, 'logit_learning_curves')
+        self._plot(plt, 'learning_curves')
         print('Done')
 
 
